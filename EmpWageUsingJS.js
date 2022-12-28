6d0f1746687497cf0a73109b42021134125bb730
Vinay <navik.vinay12@gmail.com>
2022-12-28 13:26:25 +0530

    welcome initial commit

---
+++ EmpWageUsingJS.js
@@ -0,0 +1,8 @@
+//UC1 Emp is present or absent 
+
+let present=1;
+let empInput= (Math.floor(Math.random()*10)%2);  
+if(empInput==present)
+    console.log("Employee is Present");
+else
+    console.log("Employee is Absent");
