791a45d1abc330c8aba2b79ba583977a93421de1
Vinay <navik.vinay12@gmail.com>
2022-12-28 13:35:40 +0530

    calculating emp wage based on part or full time or absent

--- EmpWageUsingJS.js
+++ EmpWageUsingJS.js
@@ -1,9 +1,27 @@
 //Welcome to Employee Wage Program Using JS.
 //UC1 Emp is present or absent 
+//UC2 Ability to calculate Daily Emp Wage based on Full Time/Part Time or Absent.
 
+const Is_FULL_TIME=1;
+const Is_PART_TIME=2;
+const EMP_RATE_PER_HR=20;
+let empHrs=0;
 let present=1;
-let empInput= (Math.floor(Math.random()*10)%2);  
-if(empInput==present)
-    console.log("Employee is Present");
-else
-    console.log("Employee is Absent");
+let empWage=0;
+let empInput= (Math.floor(Math.random()*10)%3);  
+switch(empInput)
+{
+    case Is_FULL_TIME:
+        console.log("Full Time Employee is present ")
+        empHrs=8;
+        break;
+    case Is_PART_TIME:
+        console.log("Part Time Employee is present ")
+        empHrs=4
+        break;
+    default:
+        console.log("Employee is Absent")
+        break;
+}
+empWage=EMP_RATE_PER_HR*empHrs;
+console.log("Employee Wage is :"+empWage);
