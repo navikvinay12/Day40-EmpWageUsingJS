beca261aee01307a8a2315e71529dc822fca38a7
Vinay <navik.vinay12@gmail.com>
2022-12-29 12:25:14 +0530

    storing daily wage in an Array

--- EmpWageUsingJS.js
+++ EmpWageUsingJS.js
@@ -1,4 +1,4 @@
-//UC5 Calculating Emp Wage with given Max working hrs and max workng days .
+//UC6 Storing Daily Wage in an Array.
 const Is_FULL_TIME=1;
 const Is_PART_TIME=2;
 const EMP_RATE_PER_HR=20;
@@ -11,6 +11,7 @@ let empHrs=0;
 let totalEmpHrs=0;
 let totalWage=0;
 let day=1;
+let empWageArray=new Array();       //creating Array
 
 function GetWorkingHrs()
 {
@@ -33,8 +34,10 @@ while(day<=MAX_WORKING_DAY && totalEmpHrs<=MAX_WORKING_HRS)
     empHrs=GetWorkingHrs()
     empWage=EMP_RATE_PER_HR*empHrs;
     console.log("Day# : "+day+" EmpWage : "+empWage)
+    empWageArray.push(empWage);     //Store everyday wage in an Array
     totalWage+=empWage
     totalEmpHrs+=empHrs
     day++;
 }
+console.log("UC6-Array Object for Daily Wage :"+ empWageArray );    //UC6 printing Daiy wage.
 console.log("Total Employee Wage for "+(day-1)+" days & Emp Hrs "+ (totalEmpHrs-empHrs) +" is :"+(totalWage));
