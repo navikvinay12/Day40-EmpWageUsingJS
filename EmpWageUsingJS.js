195b86ddaa36b96237fdb79e139baa23fa0dca96
Vinay <navik.vinay12@gmail.com>
2022-12-29 16:14:28 +0530

    7 operations based on some helper functions using Array

--- EmpWageUsingJS.js
+++ EmpWageUsingJS.js
@@ -1,4 +1,14 @@
-//UC6 Storing Daily Wage in an Array.
+/*
+UC7
+a)Calc total Wage using Array forEach or reduce method
+b)Show the Day along with Daily Wage using Array map helper function
+c)Show Days when Full Time wage of 160 were earned using filer function
+d)Find the first occurence when full time wage was earned using find function
+e)Check if Every Element of Full Time Wage is truly holding Full time wage
+f)Check if is any Part Time Wage
+g)Find the number of days the Employee Worked.
+*/
+
 const Is_FULL_TIME=1;
 const Is_PART_TIME=2;
 const EMP_RATE_PER_HR=20;
@@ -19,13 +29,13 @@ function GetWorkingHrs()
     switch(empInput)
     {
         case Is_FULL_TIME:
-            console.log("Full Time Employee is present ")
+            // console.log("Full Time Employee is present ")
             return empHrs=8;
         case Is_PART_TIME:
-            console.log("Part Time Employee is present ")
+            // console.log("Part Time Employee is present ")
             return empHrs=4
         default:
-            console.log("Employee is Absent")
+            // console.log("Employee is Absent")
             return empHrs=0
     }
 }
@@ -41,3 +51,60 @@ while(day<=MAX_WORKING_DAY && totalEmpHrs<=MAX_WORKING_HRS)
 }
 console.log("UC6-Array Object for Daily Wage :"+ empWageArray );    //UC6 printing Daiy wage.
 console.log("Total Employee Wage for "+(day-1)+" days & Emp Hrs "+ (totalEmpHrs-empHrs) +" is :"+(totalWage));
+
+//UC7A Way1 -using foreach
+let total=0;
+function getTotalWage(dailyWage)
+{
+    total+=dailyWage
+}
+empWageArray.forEach(getTotalWage)
+console.log("UC-7A Calculating Total Emp Wage using array helper function i.e foreach : "+total);
+//UC7A Way2 - reduce
+function getTotalWageUsingReduce(totalWage,dailyWage)
+{
+    return totalWage+=dailyWage
+}
+console.log("UC-7A Calculating Total Emp Wage using array helper function i.e reduce : "+empWageArray.reduce(getTotalWageUsingReduce,0))
+
+//UC7B Represent day with wage.
+let dayCount=0;
+function mapDayWithWageFunc(dailyWage)
+{
+    dayCount++
+    return "Day"+dayCount+":"+dailyWage;
+}
+mapDayAlongWithWage = empWageArray.map(mapDayWithWageFunc)
+console.log("UC-7B Represent day with wage using map helper function:\n"+mapDayAlongWithWage)
+
+//UC7C Show FullTime days.
+function FullTimeDays(data)
+{
+    return data.includes("160")
+}
+let fullTimeDays=mapDayAlongWithWage.filter(FullTimeDays)
+console.log("UC-7C Show FullTime Days using filter helper function:\n"+fullTimeDays)
+
+//UC7D Show First day of Occurance of Full time work.
+console.log("UC-7D Show First Occurance of full time day using find helper function :- "+fullTimeDays.find(FullTimeDays))
+
+//UC7E Check every element is full time or not using every helper function
+console.log("UC-7E Check every element is full time or not? using every helper function :- "+fullTimeDays.every(FullTimeDays))   //true
+console.log("UC-7E Check every element is full time or not? using every helper function :- "+mapDayAlongWithWage.every(FullTimeDays))    //false
+
+//UC7F Check if any part time wage is present or not?
+function PartTimeDays(data)
+{
+    return data.includes("80")
+}
+console.log("UC-7F Check if any part time wage is present or not? using some helper function :- "+mapDayAlongWithWage.some(PartTimeDays))
+
+//UC7G Number of Present Days .
+function NumberOfDays(total,dailyWage)
+{
+    if(dailyWage>0)
+        return total=total+1;
+    else
+        return total;
+}
+console.log("UC-7G Number of Working days using reduce helper function :- "+empWageArray.reduce(NumberOfDays,0))
