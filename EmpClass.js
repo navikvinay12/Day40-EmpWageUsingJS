42f0e54b3a0411f312e3c2f44718ba304fe85cdd
navikvinay12 <Navik.vinay12@gmail.com>
2023-01-08 11:49:34 +0530

    UC12 -added more data in the class -gender and start date.

--- EmpClass.js
+++ EmpClass.js
@@ -1,16 +1,21 @@
 //UC11 Ability to create Employee Payrol Data with id,name and salary .
+//UC12 Abiity to extend Employee Payroll Data to store gender and start date.
 class EmployeePayrollData
 {
     //property
     id;
     salary;
+    gender;         
+    startDate;
 
     //constructor
-    constructor(id,name,salary)
+    constructor(...params)
     {
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.startDate=params[4];
     }
 
     //getter and setter method
@@ -20,12 +25,24 @@ class EmployeePayrollData
     //method
     toString()
     {
-        return "id="+this.id+", name="+this.name+", salary="+this.salary;
+        const options = { day:'numeric' , month:'long', year: 'numeric'};
+        const empdate=!this.startDate ? "undefinedDate" : 
+        this.startDate.toLocaleDateString("en-US",options);
+        return "id="+this.id+", name="+this.name+", salary="+this.salary+", gender="+this.gender+", startDate="+empdate ;
     }
 }
 
-let employeePayrollData=new EmployeePayrollData(1,"Mark",30333);
-console.log(employeePayrollData);
+let employeePayrollData=new EmployeePayrollData(1);
+console.log(employeePayrollData.toString());
+
+employeePayrollData=new EmployeePayrollData(1,"Mark",30000);
+console.log(employeePayrollData.toString());
+
 employeePayrollData.name="john";
 employeePayrollData.salary=44333;
-console.log(employeePayrollData);
+employeePayrollData.gender="M";
+employeePayrollData.startDate;
+console.log(employeePayrollData.toString());
+
+employeePayrollData.startDate=new Date();
+console.log(employeePayrollData.toString());
