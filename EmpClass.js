d01663dfe5d9b404033a5c55179db80795e91ddc
navikvinay12 <Navik.vinay12@gmail.com>
2023-01-08 15:19:49 +0530

    regex applied to each data members in the class

--- EmpClass.js
+++ EmpClass.js
@@ -3,12 +3,13 @@
 //UC13 Use Regex Pattern and Use TryCatch in case of Error.
 class EmployeePayrollData
 {
-    //property
+    //property (even if it is not written nothing will be impacted)
     id;
     salary;
     gender;         
-    startDate;
-
+    // startDate;
+    // email;
+    // pinCode;
     //constructor
     constructor(...params)
     {
@@ -17,9 +18,23 @@ class EmployeePayrollData
         this.salary=params[2];
         this.gender=params[3];
         this.startDate=params[4];
+        this.email=params[5];
+        this.pinCode=params[6];
     }
-
     //getter and setter method
+    get id() {return this._id; }
+    set id(id)
+    {
+        let idRegex=RegExp("^[1-9]{1}[0-9]*$");
+        if(idRegex.test(id))
+        {
+            this.id=id;
+        }
+        else 
+        {
+            throw "ID is Incorrect";
+        }
+    }
     get name()  { return this._name;}
     set name(name)
     {
@@ -33,28 +48,95 @@ class EmployeePayrollData
             throw "Name is Incorrect";
         }
     }
-
+    get salary()    { return this._salary}
+    set salary(salary)
+    {
+        let salaryRegex=RegExp("^[1-9]{1}[0-9]*([.]?)[0-9]+$");
+        if(salaryRegex.test(salary))
+        {
+            this._salary=salary;
+        }
+        else
+        {
+            throw "Salary is Incorrect";
+        }
+    }
+    get gender()    { return this._gender}
+    set gender(gender)
+    {
+        let genderRegex=RegExp("^[MF]{1}$");
+        if(genderRegex.test(gender))
+        {
+            this._gender=gender;
+        }
+        else
+        {
+            throw "Invalid Gender Input!";
+        }
+    }
+    get startDate()    { return this._startDate}
+    set startDate(startDate)
+    {
+        let date=new Date();
+        let startDateRegex=RegExp("^[0-9]{1,2}[/][0-9]{1,2}[/][0-9]{4}$");
+        if(startDate <= date.toLocaleDateString())
+        {
+            if(startDateRegex.test(startDate))
+                this._startDate=startDate;
+            else
+                throw "Invalid Format";
+        }
+        else
+        {
+            throw "Future Date can't be set!";
+        }
+    }
+    get email()    { return this._email}
+    set email(email)
+    {
+        let emailRegex=RegExp("^[a-zA-Z0-9]{1,}([._+-]{1}[a-zA-Z0-9]{1,}){0,1}[@]{1}[a-z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-z]{2,3}){0,1}$");
+        if(emailRegex.test(email))
+        {
+            this._email=email;
+        }
+        else
+        {
+            throw "Invalid Email ";
+        }
+    }
+    get pinCode()    { return this._pinCode}
+    set pinCode(pinCode)
+    {
+        let pinCodeRegex=RegExp("^[1-9]{1}[0-9]{2}([ ]{0,1})[0-9]{3}$");
+        if(pinCodeRegex.test(pinCode))
+        {
+            this._pinCode=pinCode;
+        }
+        else
+        {
+            throw "Invalid PinCode!";
+        }
+    }
     //method
     toString()
     {
-        const options = { day:'numeric' , month:'long', year: 'numeric'};
-        //const empdate=!this.startDate ? "undefinedDate" : this.startDate.toLocaleDateString("en-US",options);
-        const empdate=this.startDate === undefined ? "undefinedDate" : this.startDate.toLocaleDateString("en-US",options);
-        return "id="+this.id+", name="+this.name+", salary="+this.salary+", gender="+this.gender+", startDate="+empdate ;
+        // const options = { day:'numeric' , month:'long', year: 'numeric'};
+        // //const empdate=!this.startDate ? "undefinedDate" : this.startDate.toLocaleDateString("en-US",options);
+        // const empdate=this.startDate === undefined ? "undefinedDate" : this.startDate.toLocaleDateString("en-US",options);
+        return "id="+this.id+", name="+this.name+", salary="+this.salary+", gender="+this.gender+", startDate="+this.startDate+
+        ", email:"+this.email+", Pincode:"+this.pinCode ;
     }
 }
-
-let employeePayrollData=new EmployeePayrollData(1,"Mark",30000);
-console.log(employeePayrollData.toString());
 try
 {
-    employeePayrollData.name="john";
+    let employeePayrollData=new EmployeePayrollData(1,"Vinay",50000,"M", "08/12/2022" , "navik.vinay12@gmail.com", "400074");
+    console.log(employeePayrollData.toString());
+    employeePayrollData=new EmployeePayrollData(2,"Vinaykumar",60000,"M", "08/01/2023" , "vinay@bridgelabz.com", "400 074");
     console.log(employeePayrollData.toString());
 }
 catch(e)
 {
     console.error(e);
 }
-employeePayrollData=new EmployeePayrollData(2,"Terrisa",50000,"F", new Date());
-console.log(employeePayrollData.toString());
+
 
